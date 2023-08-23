1215573e126eed6ab35ebef068b2da92969177e4
victor silva <canalampmoficial2018@gmail.com>
2023-08-23 17:55:54 -0300

    MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Restaurant.destroy_all
+
+Restaurant.create(name: 'Restaurant 1', address: 'Address 1', phone_number: "8854-1111", category: 'chinese')
+Restaurant.create(name: 'Restaurant 2', address: 'Address 2', phone_number: "8888-4444", category: 'italian')
+Restaurant.create(name: 'Restaurant 3', address: 'Address 3', phone_number: "7965-8888", category: 'japanese')
+Restaurant.create(name: 'Restaurant 4', address: 'Address 4', phone_number: "4541-0000", category: 'french')
+Restaurant.create(name: 'Restaurant 5', address: 'Address 5', phone_number: "4579-8888", category: 'belgian')
